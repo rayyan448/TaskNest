836892a9b5b49e7cefaec6475cd1b76947f1f524
rayyan448 <rayyanp470@gmail.com>
2025-10-16 11:01:28 +0530

    16 oct Taskform completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Taskform from './components/Taskform'
 import Tasklist from './components/Tasklist'
 import Progresstracker from './components/Progresstracker'
+import { useState } from 'react'
 function App() {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  });
+  const addTask = (task) => {
+    setTasks([...tasks, task]);
+  }
   return (
     <div>
       <h1>Task Nest</h1>
       <p>Our friendly TaskManager</p>
-      <Taskform />
+      <Taskform addTask={addTask}/>
       <Tasklist />
       <Progresstracker />
       <button>Clear All Tasks</button>
--- src/Components/Taskform.jsx
+++ src/Components/Taskform.jsx
@@ -1,18 +1,33 @@
-function TaskForm() {
+import React, { useState } from 'react'
+
+function TaskForm({addTask}) {
+    const [task, setTask] = useState("");
+    const [priority, setPriority] = useState("medium");
+    const [category, setCategory] = useState("general");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (!task.trim()) return;
+        addTask({text: task, priority, category, completed: false})
+        setTask("");
+        setPriority("medium");
+        setCategory("general");
+    }
     return (
-        <form action="">
+        <form action="" onSubmit={handleSubmit}>
             <div>
-                <input type="text" placeholder="Enter the task" />
+                <input type="text" placeholder="Enter the task" value={task} onChange={(e) => setTask(e.target.value)} />
                 <button type="submit">Add Task</button>
+                <h1>{task} {priority} {category}</h1>
             </div>
             <div>
-                <select name="" id="">
+                <select name="" id="" value={priority} onChange={(e) => setPriority(e.target.value)}>
                     <option value="high">High</option>
                     <option value="medium">Medium</option>
                     <option value="low">Low</option>
                 </select>
 
-                <select name="" id="">
+                <select name="" id="" onChange={(e) => setCategory(e.target.value)}>
                     <option value="general">General</option>
                     <option value="work">Work</option>
                     <option value="personal">Personal</option>
@@ -21,4 +36,5 @@ function TaskForm() {
         </form>
     )
 }
+
 export default TaskForm
